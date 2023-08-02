74d73629d33954074d9bc62e916368788cfc0e6c
LuongVanDat <luongvandat.it@gmail.com>
2023-08-02 16:40:13 +0700

    Done dockerize application and mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,15 +13,10 @@ import { PetsModule } from './pets/pets.module';
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    csrfPrevention: false,
+    playground: true,
   }), TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'luongvandat',
-    password: 'sapassword',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    synchronize: true,
-    database: 'petdb',
+    ...require(path.resolve('ormconfig.json')),
   }), PetsModule, OwnerModule],
   controllers: [AppController],
   providers: [AppService],
