9b23590b21363597079d70bc75e9723ad21bc9a3
LuongVanDat <luongvandat.it@gmail.com>
2023-08-02 17:14:03 +0700

    fixed smell code with sonarqube

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -4,14 +4,14 @@ import { Owner } from 'src/owner/entities/owner.entity';
 import { OwnerService } from 'src/owner/owner.service';
 import { Repository } from 'typeorm';
 import { CreatePetInput } from './dto/create-pet.input';
-import { Pet } from './entities/pet.entity';
 import { UpdatePetInput } from './dto/update-pet.input';
+import { Pet } from './entities/pet.entity';
 
 @Injectable()
 export class PetsService {
     constructor(@InjectRepository(Pet) private petsRepository: Repository<Pet>, private ownersService: OwnerService) { }
 
-    async createPet(createPetInput: CreatePetInput): Promise<Pet> {
+    async createPet(createPetInput: CreatePetInput) {
         const newPet = await this.petsRepository.create(createPetInput);
 
         return await this.petsRepository.save(newPet);
@@ -23,19 +23,18 @@ export class PetsService {
 
     findOne(id: number) {
         return this.petsRepository.findOneOrFail({
-          where: {
-            id: id
-          }
+            where: {
+                id: id
+            }
         });
-      }
+    }
 
     async getOwner(ownerId: number): Promise<Owner> {
         return await this.ownersService.findOne(ownerId);
     }
 
-    async update(id: number, updatePetInput: UpdatePetInput): Promise<Pet> {
+    async update(id: number, updatePetInput: UpdatePetInput) {
         const updatedPet = await this.petsRepository.create(updatePetInput);
-
         return await this.petsRepository.save(updatedPet);
     }
 
